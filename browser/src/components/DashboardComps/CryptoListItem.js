002290ac4f4fefedc094ec79b5efe1bbf49b8d6b
JayMCres <crescenzojason@yahoo.com>
2019-12-09 17:58:41 -0500

    Implemented color change upon realtime price update

--- browser/src/components/DashboardComps/CryptoListItem.js
+++ browser/src/components/DashboardComps/CryptoListItem.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
-import { Card, Image, Button } from "semantic-ui-react";
+import { Card, Image, Button, Statistic } from "semantic-ui-react";
 
 export default class CryptoListItem extends Component {
+  state = {
+    currentPrice: [],
+    pastPrice: []
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.setCryptoPrice(), 3000);
+  }
+
+  componentWillMount() {
+    clearInterval(this.timer);
+  }
+
+  setCryptoPrice = () => {
+    this.setState({
+      currentPrice: this.props.price,
+      pastPrice: this.state.currentPrice
+    });
+  };
+
+  handlePriceChange = () => {
+    if (this.state.currentPrice > this.state.pastPrice) {
+      return "#7FFF00";
+    } else if (this.state.currentPrice < this.state.pastPrice) {
+      return "red";
+    } else {
+      return "black";
+    }
+  };
   render() {
     console.log("Crypto Item Props", this.props);
     return (
@@ -14,7 +43,14 @@ export default class CryptoListItem extends Component {
           />
           <Card.Header>{this.props.company}</Card.Header>
           <Card.Meta>{this.props.ticker}</Card.Meta>
-          {/* <Card.Description>{this.props.overview}</Card.Description> */}
+          <Card.Description>
+            <Statistic size="tiny">
+              <Statistic.Value style={{ color: this.handlePriceChange() }}>
+                {this.props.price}
+              </Statistic.Value>
+              {/* <Statistic.Label>Price($)</Statistic.Label> */}
+            </Statistic>
+          </Card.Description>
         </Card.Content>
         <Card.Content extra>
           <div className="ui two buttons">
