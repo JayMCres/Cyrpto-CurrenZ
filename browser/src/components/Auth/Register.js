6212f08e1244a6a354b088e2a1454dbfdd1f4e2c
JayMCres <crescenzojason@yahoo.com>
2019-12-11 08:16:08 -0500

    created register functionality and validations

--- browser/src/components/Auth/Register.js
+++ browser/src/components/Auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import firebase from "../../config/firebase";
 import {
   Grid,
   Form,
@@ -18,12 +19,91 @@ export default class Register extends Component {
     password: "",
     passwordConfirmation: "",
     errors: [],
-    loading: false
-    // usersRef: firebase.database().ref("users")
+    loading: false,
+    usersRef: firebase.database().ref("users")
   };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+
+  formValid = () => {
+    let errors = [];
+    let error;
+    if (this.formEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.passwordValid(this.state)) {
+      error = { message: "Password Invalid" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  formEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  passwordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = errors => {
+    console.log(errors);
+    return errors.map((error, i) => <p key={i}>{error.message}</p>);
+  };
+
+  handleRegSubmit = event => {
+    event.preventDefault();
+    if (this.formValid()) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+          createdUser.user
+            .updateProfile({
+              displayName: this.state.username,
+              photoURL: `http://www.gravatar.com/avatar/${md5(
+                createdUser.user.email
+              )}?d=identicon`
+            })
+            .then(() => {
+              this.saveUser(createdUser).then(() => {
+                console.log("User Saved");
+              });
+            });
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false
+          });
+        });
+    }
+  };
+
+  saveUser = createdUser => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL
+    });
   };
   render() {
     const {
@@ -34,13 +114,13 @@ export default class Register extends Component {
       loading
     } = this.state;
     return (
-      <Grid textAlign="center" verticalAlign="middle" className="App">
+      <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxwidth: 450 }}>
           <Header as="h2" icon color="orange" textAlign="center">
             <Icon name="puzzle piece" color="orange" />
             Register For Crypto-Curren-Z
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handleRegSubmit} size="large">
             <Segment stacked>
               <Form.Input
                 fluid
@@ -96,6 +176,7 @@ export default class Register extends Component {
           {this.state.errors.length > 0 && (
             <Message error>
               <h3>Error</h3>
+              {this.displayErrors(this.state.errors)}
             </Message>
           )}
           <Message>
