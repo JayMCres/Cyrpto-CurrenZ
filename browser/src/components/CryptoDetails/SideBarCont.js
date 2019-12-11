4987100f3c71606b310762d52a7e7f46e822ff1d
JayMCres <crescenzojason@yahoo.com>
2019-12-11 14:25:36 -0500

    Created Weekly side price chart

--- browser/src/components/CryptoDetails/SideBarCont.js
+++ browser/src/components/CryptoDetails/SideBarCont.js
@@ -3,15 +3,31 @@ import { Segment, Header } from "semantic-ui-react";
 
 import { connect } from "react-redux";
 import NewsList from "./NewsList";
+import SidePriceChart from "./SidePriceChart";
 import UserProfile from "../DashboardComps/CryptoSideCont/UserProfile";
 class SideBarCont extends Component {
   render() {
-    console.log("Side", this.props);
-
+    // console.log("Side", this.props);
+    const sidePrices = this.props.prices.map((price, index) => {
+      return {
+        dayOne: price[0],
+        dayTwo: price[1],
+        dayThree: price[2],
+        dayFour: price[3],
+        dayFive: price[4],
+        daySix: price[5],
+        daySeven: price[6]
+      };
+    });
     return (
       <Segment inverted>
         <Segment>
           <UserProfile currentUser={this.props.currentUser} />
+        </Segment>
+        <Segment>
+          {sidePrices.map((price, index) => {
+            return <SidePriceChart key={index} {...price} />;
+          })}
         </Segment>
         <Segment>
           <NewsList
@@ -27,9 +43,9 @@ class SideBarCont extends Component {
 
 const mapStateToProps = state => ({
   news: state.cryptos.news,
-  crypto: state.cryptos.crypto
-  // details: state.cryptos.details,
-  // historicals: state.cryptos.historicals
+  crypto: state.cryptos.crypto,
+  details: state.cryptos.details,
+  prices: state.cryptos.prices
 });
 
 export default connect(mapStateToProps)(SideBarCont);
---
+++ browser/src/components/CryptoDetails/SidePriceChart.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import {
+  Segment,
+  Header,
+  Image,
+  Icon,
+  Message,
+  Button
+} from "semantic-ui-react";
+import ReactFC from "react-fusioncharts";
+import { connect } from "react-redux";
+
+// Step 3 - Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Step 4 - Include the chart type
+import Area2d from "fusioncharts/fusioncharts.charts";
+
+// Step 5 - Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Step 6 - Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Area2d, FusionTheme);
+
+// Step 7 - Creating the JSON object to store the chart configurations
+
+class SidePriceChart extends Component {
+  render() {
+    // console.log("price chart", this.props);
+    const chartConfigs = {
+      type: "area2d", // The chart type
+      width: "600", // Width of the chart
+      height: "400", // Height of the chart
+      dataFormat: "json", // Data type
+      dataSource: {
+        // Chart Configuration
+        chart: {
+          caption: "Weekly Price Change",
+          subCaption: "Crypto Price Tracker",
+          xAxisName: "Date",
+          yAxisName: "US Dollars ($)",
+          numberSuffix: "$",
+          theme: "fusion"
+        },
+        // Chart Data
+        data: [
+          {
+            label: this.props.dayOne.d,
+            value: this.props.dayOne.y
+          },
+          {
+            label: this.props.dayTwo.d,
+            value: this.props.dayTwo.y
+          },
+          {
+            label: this.props.dayThree.d,
+            value: this.props.dayThree.y
+          },
+          {
+            label: this.props.dayFour.d,
+            value: this.props.dayFour.y
+          },
+          {
+            label: this.props.dayFive.d,
+            value: this.props.dayFive.y
+          },
+          {
+            label: this.props.daySix.d,
+            value: this.props.daySix.y
+          },
+          {
+            label: this.props.daySeven.d,
+            value: this.props.daySeven.y
+          }
+        ]
+      }
+    };
+
+    return (
+      <div>
+        <ReactFC {...chartConfigs} />
+      </div>
+    );
+  }
+}
+
+export default SidePriceChart;
